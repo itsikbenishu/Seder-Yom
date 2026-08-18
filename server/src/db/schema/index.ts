16b9ceb08e67091968281b0410504f2f33b27fa8
itsikbenishu <itsikbe1@gmail.com>
2026-08-18 13:22:27 +0300

    feat: add export for googleCalendarTokens from schema

--- server/src/db/schema/index.ts
+++ server/src/db/schema/index.ts
@@ -2,3 +2,4 @@ export * from "./events.js";
 export * from "./eventFiles.js";
 export * from "./archivedDays.js";
 export * from "./notificationPreferences.js";
+export * from "./googleCalendarTokens.js";
