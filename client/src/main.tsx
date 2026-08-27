4331646a48c0dbecb6f380296e95858f7e053a80
itsikbenishu <itsikbe1@gmail.com>
2026-08-27 15:40:33 +0300

    feat: add React Query Devtools for improved debugging experience

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 import './i18n'
 import App from './App.tsx'
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 )
